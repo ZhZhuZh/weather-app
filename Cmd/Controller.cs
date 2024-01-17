46570232fcd18310ffa78b20b8109ec975ef9d43
Виктория Жукова <VYZHUKOVA@BG.loc>
2024-01-17 16:45:33 +0300

    fix

--- Cmd/Controller.cs
+++ Cmd/Controller.cs
@@ -21,13 +21,7 @@ public class Controller : ControllerBase
     [HttpGet("MoskowWeather")]
     public async Task<ActionResult<string>> GetMoscowWeather([FromQuery] string date)
     {
-        using HttpClient client = new();
-
-        string response = await client.GetStringAsync($"https://archive-api.open-meteo.com/v1/archive?latitude=55.7522&longitude=37.6156&start_date={date}&end_date={date}&hourly=temperature_2m&timezone=Europe%2FMoscow");
-        JObject joResponse = JObject.Parse(response);
-        JObject? ojObject = (JObject?)joResponse["hourly"]; 
-        JArray? array = (JArray?)ojObject?["temperature_2m"];
-        string temperature = (array?[0].ToString() ?? "0") + "°C";
+        string temperature = GetWeatherDataAndParse(date).Result + "°C";
 
         return temperature;
     }
